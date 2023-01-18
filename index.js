e318ded73932e7adde0e3a17f96995017641cbf2
wyk <122308928+wyknan@users.noreply.github.com>
2023-01-18 14:56:59 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
     
 window.onload=function(){
-    let roll = document.getElementsByClassName('roll');
-    let rollLeft = roll.firstElementChild
-    let rollRight = roll.lastElementChild
+    let roll = document.getElementById('roll');
+    let rollLeft = roll.children[0]
+    let rollRight = roll.children[1]
     
     
     setTimeout(function auto() {
@@ -12,5 +12,19 @@ window.onload=function(){
         current.classList.remove('current')
         next.classList.add('current')
         setTimeout(auto,3000)
-    },3000)
+    },3000);
+    rollLeft.onclick=function(){
+        const emg = document.getElementById('emg')
+        let current = document.querySelector(".current")
+        const previous = current.previousElementSibling || emg.getElementsByTagName("li")[3];
+        current.classList.remove('current')
+        previous.classList.add('current')
+    }
+    rollRight.onclick=function(){
+        const emg = document.getElementById('emg')
+        let current = document.querySelector(".current")
+        const next = current.nextElementSibling || emg.getElementsByTagName("li")[0];
+        current.classList.remove('current')
+        next.classList.add('current')
+    }
 }
