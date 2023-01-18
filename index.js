c164cfc494798f79dfd740b0efea5da241899291
wyk <122308928+wyknan@users.noreply.github.com>
2023-01-18 16:51:43 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,28 +3,47 @@ window.onload=function(){
     let roll = document.getElementById('roll');
     let rollLeft = roll.children[0]
     let rollRight = roll.children[1]
-    
-    
-    setTimeout(function auto() {
+    const emg = document.getElementById('emg')
+    let timer1
+    let timer2
+    let c = 'n'
+    toChange('y')
+    function toChange(c){
+        console.log(c);
+        if (c==='y') {
+            timer1 = setTimeout(function auto() {
+                togo('next')
+                timer2=setTimeout(auto,3000)
+                    },3000);
+        }else if(c==='n') {
+            clearTimeout(timer1)
+            clearTimeout(timer2)
+            timer1 = null
+            console.log();
+        }
+    }
+    emg.onmouseenter = function(){
+        toChange('n')
+    }
+    emg.onmouseleave = function(){
+        toChange('y')
+    }
+    function togo(dir) {
         const emg = document.getElementById('emg')
         let current = document.querySelector(".current")
-        const next = current.nextElementSibling || emg.getElementsByTagName("li")[0];
+        let yes
+        if(dir==='next'){
+            yes = current.nextElementSibling || emg.getElementsByTagName("li")[0];
+        }else if(dir==='prev'){
+            yes = current.previousElementSibling || emg.getElementsByTagName("li")[3];
+        }
         current.classList.remove('current')
-        next.classList.add('current')
-        setTimeout(auto,3000)
-    },3000);
+        yes.classList.add('current')
+    }
     rollLeft.onclick=function(){
-        const emg = document.getElementById('emg')
-        let current = document.querySelector(".current")
-        const previous = current.previousElementSibling || emg.getElementsByTagName("li")[3];
-        current.classList.remove('current')
-        previous.classList.add('current')
+        togo('prev')
     }
     rollRight.onclick=function(){
-        const emg = document.getElementById('emg')
-        let current = document.querySelector(".current")
-        const next = current.nextElementSibling || emg.getElementsByTagName("li")[0];
-        current.classList.remove('current')
-        next.classList.add('current')
+        togo('next')
     }
 }
