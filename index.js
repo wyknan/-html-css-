de19edf6fa6d88392e66eff172ffb987ef128f76
wyk <122308928+wyknan@users.noreply.github.com>
2023-01-18 14:10:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-let roll = document.getElementsByClassName('roll');
+    
+window.onload=function(){
+    let roll = document.getElementsByClassName('roll');
+    let rollLeft = roll.firstElementChild
+    let rollRight = roll.lastElementChild
+    
+    
+    setTimeout(function auto() {
+        const emg = document.getElementById('emg')
+        let current = document.querySelector(".current")
+        const next = current.nextElementSibling || emg.getElementsByTagName("li")[0];
+        current.classList.remove('current')
+        next.classList.add('current')
+        setTimeout(auto,3000)
+    },3000)
+}
